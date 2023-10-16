dbea1e5867fa23d181491ceb2867350cdfb378a8
Saurabh Yadav <91598832+sydeveloper3@users.noreply.github.com>
2023-10-16 23:12:42 +0530

    Add files via upload

---
+++ src/data.js
@@ -0,0 +1,52 @@
+  const reviews =
+[
+    {
+        id: 1,
+        name: "Saurabh yadav",
+        job: "Software Developer",
+        image: 'https://media.licdn.com/dms/image/D4E35AQGv8efsK34chA/profile-framedphoto-shrink_400_400/0/1693203681701?e=1698080400&v=beta&t=1aMEm2NKcAQxiyccktKDJ6gm1-U-q5PVptk_dDudJzA',
+        text : 'I have had the pleasure of working with this team on several projects,and I am consistently impressed with their technical expertise and ability to deliever quality solutions on time and within budget.They are a true partner and an asset to any project .& also looking for a oppurtinity'
+
+    },
+    {
+        id: 2,
+        name: "Uvesh Ahamed",
+        job: "Java Developer",
+        image: 'https://media.licdn.com/dms/image/D5603AQHmRgBJqeJ7EQ/profile-displayphoto-shrink_400_400/0/1680159736127?e=1703116800&v=beta&t=dVjMbq0pb_-WbahwmIy7SosMVeTncWQPK_JRKNmJYyg',
+        text : 'I have had the pleasure of working with this team on several projects,and I am consistently impressed with their technical expertise in JAVA and ability to deliever quality solutions on time and within budget.They are a true partner and an asset to any project'
+
+    },
+    {
+        id: 3,
+        name: "Priyanshu Singh",
+        job: "Software Developer",
+        image: 'https://media.licdn.com/dms/image/D4E03AQFN86rQXQYpTw/profile-displayphoto-shrink_400_400/0/1684958500270?e=1703116800&v=beta&t=7iq4_V_EvEf2EPqgTp-7jARBBtvPjHTnLx3ESIjSgIs',
+        text : 'I have had the pleasure of working with this team on several projects,and I am consistently impressed with their technical expertise in JAVA  & REACT and ability to deliever quality solutions on time and within budget.They are a true partner and an asset to any project.'
+
+    },
+    {
+        id: 4,
+        name: "Karan Singh",
+        job: "FullStack Developer",
+        image: 'https://media.licdn.com/dms/image/D4D35AQGry976xEF1JA/profile-framedphoto-shrink_400_400/0/1684393477011?e=1698080400&v=beta&t=SC_tcHxn2m6OcY6PISHCQHktYkkubcFFfjCd_WBpD2c',
+        text : 'I have had the pleasure of working with this team on several projects,and I am consistently impressed with their technical expertise and ability to deliever quality solutions on time and within budget.They are a true partner and an asset to any project Having a good hands in stock too'
+
+    },
+    {
+        id: 5,
+        name: "Aadarsh Sharma",
+        job: "Android Developer",
+        image: 'https://media.licdn.com/dms/image/D5603AQGq3wvtIO4HJw/profile-displayphoto-shrink_400_400/0/1675349122122?e=1703116800&v=beta&t=fa2OyN4VMSi5dev8SEmqIR4Z5DJ9ZKs8cOnD7HF9hxo',
+        text : 'I have had the pleasure of working with this team on several projects,and I am consistently impressed with their technical expertise in App Development  and ability to deliever quality solutions on time and within budget.They are a true partner and an asset to any project & I am MCA persuing Graduate Engineer'
+
+    },
+    {
+        id: 6,
+        name: "Sangeet Kumar",
+        job: "Mern Developer",
+        image: 'https://media.licdn.com/dms/image/D4E03AQFzwAMs0His2A/profile-displayphoto-shrink_400_400/0/1664199952101?e=1703116800&v=beta&t=r5syR_JTXWJKpuDE0KcOyTaIGQr9MdnwfQxVdoP1mWY',
+        text : 'I have had the pleasure of working with this team on several projects,and getting a hands on experience on MERN. I am consistently impressed with their technical expertise and ability to deliever quality solutions on time and within budget.They are a true partner and an asset to any project'
+
+    },
+];
+export default reviews
